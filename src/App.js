5d52b402ea3ad6732c02108644b0c94b0b74bd27
maemb19 <maemendoza19@gmail.com>
2022-09-05 00:12:41 -0300

    desafio 4 contador

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Header from './components/Header';
 import ItemListContainer from './components/ItemListContainer';
+import ItemCount from './components/ItemCount';
 
 function App() {
   return (
     <div className='container-fluid fondo_navbar'>
       <Header/>
+      <hr/>
       <ItemListContainer gretting="hola"/>
+      <ItemCount stock={5} initial={1} onAdd={0}/>
     </div>
   );
 }
---
+++ src/components/ItemCount.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+
+const ItemCount = ({stock, initial, onAdd}) => {
+  const [cantidad, setCantidad] = useState(initial);
+  const [itemStock, setItemStock] = useState(stock);
+  const [itemAdd, setItemAdd] = useState(onAdd);
+
+  const restarCantidad = (valor) => {
+    if (valor > 0){
+      setCantidad(valor);
+    }
+  }
+
+  const sumarCantidad = (valor) => {
+    if (valor <= itemStock){
+      setCantidad(valor);
+    }  
+  }
+
+  const agregarProducto = () => {
+    if (cantidad <= itemStock) {
+      setItemStock(itemStock - cantidad);
+      setItemAdd(itemAdd + cantidad);
+    }
+  }
+
+  return (
+    <div className="container py-5">
+      <div className="row">
+        <div className="col-md-2">
+          <p className="text-center">IMAGEN</p>
+          <div className="input-group">
+            <input type="button" className="btn btn-secondary" value="-" onClick={() =>
+            {restarCantidad (cantidad - 1)}}/>
+            <input type="text" className="form-control text-center" value={cantidad}/>
+            <input type="button" className="btn btn-secondary" value="+" onClick={() =>
+            {sumarCantidad (cantidad + 1)}}/>
+          </div>
+          <div className="d-grid gap-2n pt-2">
+            <input type="button" className="btn btn-danger"value="Agregar" onClick={() =>
+            {agregarProducto()}}/>
+          </div> 
+          <p>Seleccionaste {itemAdd} Productos</p>
+        </div>
+      </div>
+    </div>
+  )
+};
+
+export default ItemCount;
